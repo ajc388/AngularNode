e165755a406344f6870409cf266974ec936a9e2c
Z3R0_H0 <ajc388@nau.edu>
2015-10-01 19:42:48 -0500

    1.)Added an index page for the angular front end. 2.) Package.json now has express installed in it, but this is not needed at this point.

---
+++ index.js
@@ -0,0 +1,9 @@
+<html ng-app>
+	<head>
+		<title> Hello Angular! </title>
+		<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.2.25/angular.min.js"></script>
+	</head>
+	<body>
+		<input ng-model="name"> Hello {{ name }}
+	</body>
+</html>
